252ac133009772c8fe7cdb3554dc2cfbc7436ce7
Rishabh <70062058+rixhabhftw@users.noreply.github.com>
2024-07-04 13:08:45 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://script.google.com/macros/s/AKfycbxnQZC5-xyFQXtPsmKRNxnPqpG9U1qaUZHptenM4g9PN-wWthKsvNZHUv1dkJdO5Tzs/exec')
+    fetch('https://script.google.com/macros/s/AKfycbz8bKrhVIToCc-HvPuh7PMjJe0mZRX_gs0AbVhpAMk5EbXbeSwqlFkdRc2OMZaqmZ6yxA/exec')
         .then(response => response.json())
         .then(data => {
-            const resultsDiv = document.getElementById('results');
-            resultsDiv.innerHTML = '';
-
-            data.forEach(user => {
-                const userDiv = document.createElement('div');
-                userDiv.classList.add('user-result');
-
-                userDiv.innerHTML = `
-                    <h2>${user.name} (${user.email})</h2>
-                    <div class="category">Linguistic:</div><p>${user.linguistic}%</p>
-                    <div class="category">Logical-Mathematical:</div><p>${user.logicalMathematical}%</p>
-                    <div class="category">Musical:</div><p>${user.musical}%</p>
-                    <div class="category">Spatial:</div><p>${user.spatial}%</p>
-                    <div class="category">Bodily-Kinesthetic:</div><p>${user.bodilyKinesthetic}%</p>
-                    <div class="category">Interpersonal:</div><p>${user.interpersonal}%</p>
-                    <div class="category">Intrapersonal:</div><p>${user.intrapersonal}%</p>
-                    <div class="category">Naturalist:</div><p>${user.naturalist}%</p>
+            const resultsTableBody = document.getElementById('resultsTableBody');
+            data.forEach(result => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${result.name}</td>
+                    <td>${result.email}</td>
+                    <td>${result.linguistic}%</td>
+                    <td>${result.logicalMathematical}%</td>
+                    <td>${result.musical}%</td>
+                    <td>${result.spatial}%</td>
+                    <td>${result.bodilyKinesthetic}%</td>
+                    <td>${result.interpersonal}%</td>
+                    <td>${result.intrapersonal}%</td>
+                    <td>${result.naturalist}%</td>
                 `;
-
-                resultsDiv.appendChild(userDiv);
+                resultsTableBody.appendChild(row);
             });
+        })
+        .catch(error => {
+            console.error('Error fetching survey results:', error);
         });
 });
